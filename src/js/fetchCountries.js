f5a4ff40e20ea3959d780e9508f8d2e9fb0ac583
Yurii Denkovych <yd2004@ukr.net>
2022-06-07 20:26:13 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,10 @@
 export default function fetchCountries(name){
-    fetch(`https://restcountries.com/v3.1/name/{name}`)
+    fetch(`https://restcountries.com/v3.1/name/${name}`)
     .then(response =>{
+        if (!response.ok) {
+            throw new Error(response.status);}
         console.log(response.json())
     }).catch(console =>{
-        console.log('error')
+        
     })
 }
