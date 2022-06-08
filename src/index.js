6a1331c0eb3dbe292a02f080e69c2bc700d85f2f
Yurii Denkovych <yd2004@ukr.net>
2022-06-08 20:10:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 
 import './css/styles.css';
-import fetchCountries from './fetchCountries.js';
+//import {fetchCountries} from './fetchCountries.js';
+import countryCard from '../src/country-card.hbs';
+refs={
+    searchCountry : document.querySelector('#search-box'),
+    listCountry: document.querySelector('.country-list'),
+    infoCountry: document.querySelector('.country-info')
+}
 const DEBOUNCE_DELAY = 300;
-fetchCountries(peru);
+let name = 'ukr';
+
+fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(response =>{
+       
+    return response.json();
+    })
+    
+    .then(country=>{
+    console.log(country[0]);
+    })
+    .catch(error=>{
+        console.log(error);
+    })
