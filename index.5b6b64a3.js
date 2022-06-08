3dd91fa2bd0304b987c80a020c21850945a31880
Denkovych <95170165+Denkovych@users.noreply.github.com>
2022-06-08 18:58:09 +0000

    Deploying to gh-pages from @ Denkovych/goit-js-hw-10@cb66b89fc56158e50cd3f6b01d5554127679daae 🚀

--- index.5b6b64a3.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;n=peru,fetch("https://restcountries.com/v3.1/name/".concat(n)).then((function(n){console.log(n.json())}))}();
-//# sourceMappingURL=index.5b6b64a3.js.map
--- index.cd475d3f.js
+++
@@ -1,2 +0,0 @@
-var e;e=peru,fetch(`https://restcountries.com/v3.1/name/${e}`).then((e=>{console.log(e.json())}));
-//# sourceMappingURL=index.cd475d3f.js.map
