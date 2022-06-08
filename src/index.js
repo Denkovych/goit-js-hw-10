cb66b89fc56158e50cd3f6b01d5554127679daae
Yurii Denkovych <yd2004@ukr.net>
2022-06-08 21:57:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 
 import './css/styles.css';
 //import {fetchCountries} from './fetchCountries.js';
-import countryCard from '../src/country-card.hbs';
+
 refs={
     searchCountry : document.querySelector('#search-box'),
     listCountry: document.querySelector('.country-list'),
     infoCountry: document.querySelector('.country-info')
 }
 const DEBOUNCE_DELAY = 300;
-let name = 'ukr';
+let name = 'ukraine';
 
 fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
     .then(response =>{
@@ -17,8 +17,20 @@ fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,populatio
     })
     
     .then(country=>{
-    console.log(country[0]);
+   const markup = makeMarkupCountry(country[0]);
+   console.log(country[0].languages)
+   refs.infoCountry.innerHTML = markup;
     })
     .catch(error=>{
         console.log(error);
     })
+
+    
+function makeMarkupCountry(country){
+   
+    return  `<h2 class="country">${country.name.official}</h2>
+    <img src="${country.flags.svg}" alt="flag" width = 30px>
+<p class="info-country">Capital: ${country.capital}</p>
+<p class="info-country">Population: ${country.population}</p>
+<p class="info-country">Languages: ${country.languages}</p>`
+}
