2626dd2ec0dd4a9037f9fb2355b5ba795a1fa8f5
Yurii Denkovych <yd2004@ukr.net>
2022-06-08 20:08:35 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,17 @@
- export default function fetchCountries(name){
-   return fetch(`https://restcountries.com/v3.1/name/${name}`)
+   function fetchCountries(name){
+   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
     .then(response =>{
        
-        console.log(response.json())
+    return response.json();
+    })
+    
+    .then(country=>{
+    console.log(country[0].flags.svg);
+    })
+    .catch(error=>{
+        console.log(error);
     })
    
 }
+
+//export {fetchCountries};
