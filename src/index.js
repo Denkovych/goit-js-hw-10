310dd0fca4ca443078bc291c34304c133a75e9aa
Yurii Denkovych <yd2004@ukr.net>
2022-06-09 20:48:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,24 @@
-
+var debounce = require('lodash.debounce');
+const DEBOUNCE_DELAY = 300;
 import './css/styles.css';
-//import {fetchCountries} from './fetchCountries.js';
-
-refs={
+import {fetchCountryByName, makeMarkupCountry,renderCountryCard,makeMarkupCountryList} from './fetchCountries.js';
+export {refs};
+const refs={
     searchCountry : document.querySelector('#search-box'),
     listCountry: document.querySelector('.country-list'),
     infoCountry: document.querySelector('.country-info')
 }
-const DEBOUNCE_DELAY = 300;
-let name = 'ukraine';
 
-fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-    .then(response =>{
-       
-    return response.json();
-    })
-    
-    .then(country=>{
-   const markup = makeMarkupCountry(country[0]);
-   console.log(country[0].languages)
-   refs.infoCountry.innerHTML = markup;
-    })
-    .catch(error=>{
-        console.log(error);
-    })
 
-    
-function makeMarkupCountry(country){
-   
-    return  `<h2 class="country">${country.name.official}</h2>
-    <img src="${country.flags.svg}" alt="flag" width = 30px>
-<p class="info-country">Capital: ${country.capital}</p>
-<p class="info-country">Population: ${country.population}</p>
-<p class="info-country">Languages: ${country.languages}</p>`
+refs.searchCountry.addEventListener('input',debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(e){
+    e.preventDefault()
+    if(e.target.value !== '')
+    {console.log(e.target.value)
+    name = e.target.value.trim();
+    fetchCountryByName(name);}
 }
+
+
+
